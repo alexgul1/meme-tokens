b1952d4372cf6b639b9db8ba36a57b8908256f96
oleksandr.hulitskyi <alexgulitskyy3@gmail.com>
2024-08-14 15:42:43 +0300

    Update TelegramUserServiceV2.ts

--- src/telegram/services/TelegramUserServiceV2.ts
+++ src/telegram/services/TelegramUserServiceV2.ts
@@ -62,6 +62,8 @@ export class TelegramUserServiceV2 {
 		const message = event.message;
 		const channelId = (message?.peerId as PeerChannel)?.channelId?.toString() || '';
 
+		console.log(channelId, this.chatIds.has(channelId))
+
 		if (this.chatIds.has(channelId)) {
 
 			const extractedData = extractSolOrPairAddress(message.message);

82167d33ceb01a17db81a9e94427ace977e819fe
oleksandr.hulitskyi <alexgulitskyy3@gmail.com>
2024-08-14 14:26:12 +0300

    Update addressExtractor.ts

--- src/telegram/utils/addressExtractor.ts
+++ src/telegram/utils/addressExtractor.ts
@@ -1,4 +1,12 @@
 export const extractSolOrPairAddress = (message: string): { address: string; type: 'token' | 'pair' } | null => {
+	// Regular expression for optional parts in Dextools URL
+	const dextoolsPattern = /https:\/\/(?:www\.)?dextools\.io(?:\/app\/en\/solana)?\/pair-explorer\/([1-9A-HJ-NP-Za-km-z]{32,44})/;
+	const dextoolsMatch = message.match(dextoolsPattern);
+
+	if (dextoolsMatch) {
+		return { address: dextoolsMatch[1], type: 'pair' };
+	}
+
 	// Regular expression for Dexscreener pair URL
 	const pairUrlPattern = /https:\/\/dexscreener\.com\/solana\/([1-9A-HJ-NP-Za-km-z]{32,44})/;
 	const pairMatch = message.match(pairUrlPattern);

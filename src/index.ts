f4e00a5e190515e9e4c0b51081368e2bc4490805
alexgul1 <alexgulitskyy3@gmail.com>
2024-08-26 23:32:16 +0300

    add logs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 'use strict';
 
+import './sentry/index';
+
 import dotenv from 'dotenv';
 import {TelegramUserServiceV2} from './telegram/services/TelegramUserServiceV2';
 import {SubscriberServiceV2} from './subscriber/service/SubscriberServiceV2';
---
+++ src/sentry/index.ts
@@ -0,0 +1,6 @@
+import * as Sentry from '@sentry/node';
+
+// Ensure to call this before importing any other modules!
+Sentry.init({
+	dsn: 'https://f8cc0002892f7d401b3439acd83624ef@o4507845523275776.ingest.de.sentry.io/4507845531336784',
+});

df1ba1b105df96f2cbdd3c82ccea5cc9c188e3a4
alexgul1 <alexgulitskyy3@gmail.com>
2024-09-04 12:48:18 +0300

    Update RaydiumSwap.ts

--- src/swap/service/RaydiumSwap.ts
+++ src/swap/service/RaydiumSwap.ts
@@ -216,7 +216,7 @@ class RaydiumSwap {
 		const amountIn = new TokenAmount(currencyIn, rawAmountIn, false)
 		const currencyOut = new Token(TOKEN_PROGRAM_ID, currencyOutMint, currencyOutDecimals)
 
-		const slippage = new Percent(5, 100) // 1% slippage
+		const slippage = new Percent(1, 100) // 1% slippage
 
 		const {amountOut, minAmountOut, currentPrice, executionPrice, priceImpact, fee} = Liquidity.computeAmountOut({
 			poolKeys,

dbb40ad7e15ae24d1b71673f8d8f47ed6cf79951
alexgul1 <alexgulitskyy3@gmail.com>
2025-01-23 15:10:29 +0200

    remove call channel logic

--- src/subscriber/service/SubscriberServiceV2.ts
+++ src/subscriber/service/SubscriberServiceV2.ts
@@ -9,7 +9,7 @@ import {
 } from '../utils/isCurrentDateGreaterThanEndDate';
 import {SolanaService} from '../../solana/services/SolanaService';
 import RaydiumSwap, {sleep} from '../../swap/service/RaydiumSwap';
-import {SHOULD_SWAP, telegramUserService} from '../../index';
+import {SHOULD_SWAP} from '../../index';
 import {TelegramMessageInfo} from '../../telegram/services/TelegramUserServiceV2';
 import {MessageParams, TelegramBotService} from '../../telegram/services/TelegramServices';
 
@@ -222,18 +222,6 @@ export class SubscriberServiceV2 {
 
 	public static async putIntoDBInfoMessage(isIncluded: boolean, hasTokenAddress: boolean): Promise<void> {
 		await this.mongoDBInstance.insertTelegramMessageInfo(isIncluded, isIncluded && hasTokenAddress)
-	}
-
-	public static async getCallToChannel(token: Token, tokenName: string): Promise<void> {
-		const isExists = await this.mongoDBInstance.checkIsCallExists({address: token.address})
-
-		if (isExists) {
-			console.log('Call Exists')
-			return;
-		}
-
-		this.mongoDBInstance.addCallToDB(token)
-		telegramUserService.sendMessageToCallChannel(token.name, tokenName, token.address)
 	}
 }
 
--- src/telegram/services/TelegramUserServiceV2.ts
+++ src/telegram/services/TelegramUserServiceV2.ts
@@ -10,7 +10,6 @@ import {extractSolAddress} from '../utils/addressExtractor';
 import { EditedMessageEvent} from 'telegram/events/EditedMessage';
 import {SubscriberServiceV2} from '../../subscriber/service/SubscriberServiceV2';
 import {resolve} from 'path';
-import {generateDetailedMemeTokenPromo} from '../../openai/utils/generateDetailedMemeTokenPromo';
 import  {Cache, CacheClass} from 'memory-cache';
 import long = Api.long;
 
@@ -31,8 +30,6 @@ export class TelegramUserServiceV2 {
 	private readonly chatIds: Set<string>;
 	private readonly bannedChatIds: Set<string>;
 	private readonly processedAddresses: Set<string>;
-	// private callChannelEntity: Entity;
-	private callChannelEntity: any;
 	private readonly channelsIdToNameMap: CacheClass<long, string>
 
 	constructor() {
@@ -78,7 +75,6 @@ export class TelegramUserServiceV2 {
 	public async start(): Promise<void> {
 		await this.connect();
 		await this.handleUpdates()
-		this.callChannelEntity = await this.client.getEntity('SaulSignals')
 	}
 
 	public async eventHandle(event: NewMessageEvent) {
@@ -177,24 +173,6 @@ export class TelegramUserServiceV2 {
 				this.processedAddresses.delete(extractedData);
 			}
 		}
-	}
-
-	public async sendMessageToCallChannel(ticker: string, tokenName:string, ca: string) {
-		const promoText = await generateDetailedMemeTokenPromo(ticker, tokenName) || 'aped some **$${ticker}**. Be safe with entries.'
-
-		const messageText = `🔥 (SOL) **$${ticker}**
-Saul Signals\n
-${promoText}\n
-**CA:** \`${ca}\`\n
-https://dexscreener.com/solana/${ca}\n
-@SaulSignals
-    `;
-
-		await this.client.sendMessage(this.callChannelEntity,{
-			message: messageText,
-			file:resolve(__dirname, '../../images/saulSignal.jpeg'),
-		});
-
 	}
 
 	private async getUsername(peerChannel: PeerChannel): Promise<string> {
